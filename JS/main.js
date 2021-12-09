76221fda96898f475d521c7091a8c56bc284e074
AnotherCupOfTea <erjanengland@gmail.com>
2021-12-09 19:38:29 +0600

    Starting on Create

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+// ! Create
+
+let inputName = $(".inp-name");
+let inputSurname = $(".inp-surname");
+let inputPhone = $(".inp-phone");
+let inputWeekKpi = $(".inp-week_KPI");
+let inputMonthKpi = $(".inp-month_KPI");
+
+function addUser(API) {}
