e837fd0a5599db3cda09aea3cf420cc78acaa9cb
AnotherCupOfTea <erjanengland@gmail.com>
2021-12-09 20:13:45 +0600

    Almost done Create

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,12 @@
 // ! Create
+let formSend = $(".form-send");
 
 let inputName = $(".inp-name");
 let inputSurname = $(".inp-surname");
 let inputPhone = $(".inp-phone");
 let inputWeekKpi = $(".inp-week_KPI");
 let inputMonthKpi = $(".inp-month_KPI");
+let inputImage = $(".inp-image");
 
 function addUser(event) {
   event.preventDefault();
@@ -13,4 +15,23 @@ function addUser(event) {
   let phone = inputPhone.val().trim();
   let weekKpi = inputWeekKpi.val().trim();
   let monthKpi = inputMonthKpi.val().trim();
+  let image = inputImage.val().trim();
+
+  let newUser = {
+    name,
+    surname,
+    phone,
+    weekKpi,
+    monthKpi,
+    image,
+  };
+
+  for (let key in newUser) {
+    if (!newUser[key]) {
+      alert("Заполните поля");
+      return;
+    }
+  }
 }
+
+formSend.on("submit", addUser);
